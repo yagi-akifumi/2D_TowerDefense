f524dcdee93a2e637a4c03c93c9b887b891cde0e
八木暁史 <>
2022-05-04 19:00:16 +0900

    手順１９　－PlacementCharaSelectPopUp ゲームオブジェクトの生成処理の実装－

--- Assets/Scripts/CharaGenerator.cs
+++ Assets/Scripts/CharaGenerator.cs
@@ -11,15 +11,18 @@ public class CharaGenerator : MonoBehaviour
     [SerializeField]
     private Grid grid;         // Base 側の Grid を指定する 
 
-
-    ////*  新しい変数の宣言を１つ追加  *////
-
-
     [SerializeField]
     private Tilemap tilemaps;   // Walk 側の Tilemap を指定する
 
+    [SerializeField]
+    private PlacementCharaSelectPopUp placementCharaSelectPopUpPrefab;　　　//　PlacementCharaSelectPopUp プレファブゲームオブジェクトをアサイン用
 
-    ////*  ここまで  *////
+    [SerializeField]
+    private Transform canvasTran;　　　　　　　　　　　　　　　　　　　　　 //　PlacementCharaSelectPopUp ゲームオブジェクトの生成位置の登録用
+
+    private PlacementCharaSelectPopUp placementCharaSelectPopUp;　　　　　　//　生成された PlacementCharaSelectPopUp ゲームオブジェクトを代入するための変数
+
+    private GameManager gameManager;
 
     private Vector3Int gridPos;
 
@@ -28,38 +31,26 @@ public class CharaGenerator : MonoBehaviour
     {
         // TODO 配置できる最大キャラ数に達している場合には配置できない
 
-        // 画面をタップ(マウスクリック)したら
-        if (Input.GetMouseButtonDown(0))
+        // 画面をタップ(マウスクリック)し、かつ、配置キャラポップアップが非表示状態なら
+        if (Input.GetMouseButtonDown(0) && !placementCharaSelectPopUp.gameObject.activeSelf)
         {
 
             // タップ(マウスクリック)の位置を取得してワールド座標に変換し、それをさらにタイルのセル座標に変換
             gridPos = grid.WorldToCell(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-
-
-            ////*  新しい処理を追加  *////
-
 
             // タップした位置のタイルのコライダーの情報を確認し、それが None であるなら
             if (tilemaps.GetColliderType(gridPos) == Tile.ColliderType.None)
             {
 
                 // キャラ生成処理をメソッド化
-                CreateChara(gridPos);
+                //CreateChara(gridPos);
 
-                // TODO 配置キャラ選択用ポップアップの表示
-
+                // 配置キャラ選択用ポップアップの表示
+                ActivatePlacementCharaSelectPopUp();
             }
-
-
-            ////*  ここまで  *////
-
-
         }
+
     }
-
-
-    ////*  メソッドを１つ追加  *////
-
 
     /// <summary>
     /// キャラ生成
@@ -74,9 +65,86 @@ public class CharaGenerator : MonoBehaviour
         // キャラの位置がタイルの左下を 0,0 として生成しているので、タイルの中央にくるように位置を調整
         chara.transform.position = new Vector2(chara.transform.position.x + 0.5f, chara.transform.position.y + 0.5f);
     }
+    /// <summary>
+    /// 設定
+    /// </summary>
+    /// <param name="gameManager"></param>
+    /// <returns></returns>
+    public IEnumerator SetUpCharaGenerator(GameManager gameManager)
+    {
+
+        this.gameManager = gameManager;
+
+        // TODO ステージのデータを取得
 
 
-    ////*  ここまで  *////
+        // TODO キャラのデータをリスト化
 
+
+        // キャラ配置用のポップアップの生成
+        yield return StartCoroutine(CreatePlacementCharaSelectPopUp());
+    }
+
+
+    /// <summary>
+    /// 配置キャラ選択用ポップアップ生成
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator CreatePlacementCharaSelectPopUp()
+    {
+
+        // ポップアップを生成
+        placementCharaSelectPopUp = Instantiate(placementCharaSelectPopUpPrefab, canvasTran, false);
+
+        // ポップアップの設定
+        // TODO あとでキャラ設定用の情報も渡す
+        placementCharaSelectPopUp.SetUpPlacementCharaSelectPopUp(this);
+
+        // ポップアップを非表示にする
+        placementCharaSelectPopUp.gameObject.SetActive(false);
+
+        yield return null;
+    }
+
+
+    /// <summary>
+    /// 配置キャラ選択用のポップアップの表示
+    /// </summary>
+    public void ActivatePlacementCharaSelectPopUp()
+    {
+
+        // TODO ゲームの進行状態をゲーム停止に変更
+
+
+        // TODO すべての敵の移動を一時停止
+
+
+        // 配置キャラ選択用のポップアップの表示
+        placementCharaSelectPopUp.gameObject.SetActive(true);
+        placementCharaSelectPopUp.ShowPopUp();
+    }
+
+    /// <summary>
+    /// 配置キャラ選択用のポップアップの非表示
+    /// </summary>
+    public void InactivatePlacementCharaSelectPopUp()
+    {
+
+        // 配置キャラ選択用のポップアップの非表示
+        placementCharaSelectPopUp.gameObject.SetActive(false);
+
+
+        // TODO ゲームオーバーやゲームクリアではない場合
+
+
+        // TODO ゲームの進行状態をプレイ中に変更して、ゲーム再開
+
+
+        // TODO すべての敵の移動を再開
+
+
+        // TODO カレンシーの加算処理を再開
+
+    }
 
 }
