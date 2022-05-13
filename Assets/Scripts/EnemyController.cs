f6d11680101aa40d7cb004bf38e77e07f08aeb31
八木暁史 <>
2022-05-13 20:24:15 +0900

    手順１０　ー敵キャラの移動と移動アニメの同期処理の実装ー

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using System.Linq;
+
 
 public class EnemyController : MonoBehaviour
 {
@@ -11,20 +13,42 @@ public class EnemyController : MonoBehaviour
     [SerializeField, Header("移動速度")]
     private float moveSpeed;
 
-    private Vector3[] paths;     // 移動する各地点を代入するための配列
+    private Vector3[] paths;
+
+    private Animator anim;　　　　　　 // Animator コンポーネントの取得用
+
 
     void Start()
     {
-        // 移動する地点を取得するための配列の初期化
-        paths = new Vector3[pathData.pathTranArray.Length];
+        // Animator コンポーネントを取得して anim 変数に代入
+        TryGetComponent(out anim);
 
-        // 移動する位置情報を順番に配列に取得
-        for (int i = 0; i < pathData.pathTranArray.Length; i++)
-        {
-            paths[i] = pathData.pathTranArray[i].position;
-        }
+        // 移動する地点を取得
+        paths = pathData.pathTranArray.Select(x => x.position).ToArray();
 
         // 各地点に向けて移動
-        transform.DOPath(paths, 1000 / moveSpeed).SetEase(Ease.Linear);
+        transform.DOPath(paths, 1000 / moveSpeed).SetEase(Ease.Linear).OnWaypointChange(ChangeAnimeDirection);
+    }
+
+    /// <summary>
+    /// 敵の進行方向を取得して、移動アニメと同期
+    /// </summary>
+    private void ChangeAnimeDirection(int index)
+    {
+
+        // 次の移動先の地点がない場合には、ここで処理を終了する
+        if (index >= paths.Length)
+        {
+            return;
+        }
+
+        // 目標の位置と現在の位置との距離と方向を取得し、正規化処理を行い、単位ベクトルとする(方向の情報は持ちつつ、距離による速度差をなくして一定値にする)
+        Vector3 direction = (transform.position - paths[index]).normalized;
+        Debug.Log(direction);
+
+        // アニメーションの Palameter の値を更新し、移動アニメの BlendTree を制御して移動の方向と移動アニメを同期
+        anim.SetFloat("X", direction.x);
+        anim.SetFloat("Y", direction.y);
+
     }
 }
