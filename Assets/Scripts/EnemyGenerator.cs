8955c9b6c819e873c7e37cd2cc4a4ac2f42bcc3e
八木暁史 <>
2022-05-14 14:45:29 +0900

    手順１５　ー異なる生成地点での敵キャラの自動生成処理の実装ー

--- Assets/Scripts/EnemyGenerator.cs
+++ Assets/Scripts/EnemyGenerator.cs
@@ -9,7 +9,7 @@ public class EnemyGenerator : MonoBehaviour
     private EnemyController enemyControllerPrefab;
 
     [SerializeField]
-    private PathData pathData;
+    private PathData[] pathDatas;
 
     [SerializeField]
     private DrawPathLine pathLinePrefab;
@@ -63,11 +63,15 @@ public class EnemyGenerator : MonoBehaviour
     /// </summary>
     public void GenerateEnemy()
     {
-        // 指定した位置に敵を生成
-        EnemyController enemyController = Instantiate(enemyControllerPrefab, pathData.generateTran.position, Quaternion.identity);
+        // ランダムな値を配列の最大要素数内で取得
+        int randomValue = Random.Range(0, pathDatas.Length);　　　//　<=　☆①　処理を追加します
 
-        // 移動する地点を取得(<=　いままでEnemyController スクリプト内で行っていた処理をこちらに移動します)
-        Vector3[] paths = pathData.pathTranArray.Select(x => x.position).ToArray();
+        // 指定した位置に敵を生成
+        EnemyController enemyController = Instantiate(enemyControllerPrefab, pathDatas[randomValue].generateTran.position, Quaternion.identity);
+
+        // 移動する地点を取得
+        Vector3[] paths = pathDatas[randomValue].pathTranArray.Select(x => x.position).ToArray();
+
 
         // 敵キャラの初期設定を行い、移動を一時停止しておく
         enemyController.SetUpEnemyController(paths);
