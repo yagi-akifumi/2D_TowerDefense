20a01d4984430989f5f1df1a614840368e758b4b
八木暁史 <>
2022-05-14 09:00:06 +0900

    手順１２　ー味方キャラの攻撃処理と敵キャラのダメージ処理の実装ー

--- Assets/Scripts/CharaController.cs
+++ Assets/Scripts/CharaController.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class CharaController : MonoBehaviour
 {
+    [SerializeField, Header("攻撃力")]
+    private int attackPower = 5;
+
+    [SerializeField, Header("攻撃するまでの待機時間")]
+    private float intervalAttackTime = 60.0f;
+
+    [SerializeField]
+    private bool isAttack;
+
+    [SerializeField]
+    private EnemyController enemy;
+
     private void OnTriggerStay2D(Collider2D collision)
     {
 
@@ -11,16 +23,82 @@ public class CharaController : MonoBehaviour
         if (collision.tag == "Enemy")
         {
 
-            Debug.Log("敵発見");
+            // 攻撃中ではない場合で、かつ、敵の情報を未取得である場合
+            if (!isAttack && !enemy)
+            {
 
-            Destroy(collision.gameObject);
+                Debug.Log("敵発見");
 
-            // TODO 敵の情報(EnemyController)を取得する
+                // 敵の情報(EnemyController)を取得する。EnemyController がアタッチされているゲームオブジェクトを判別しているので、
+                // ここで、今までの Tag による判定と同じ動作で判定が行えます。
+                // そのため、☆①の処理から Tag の処理を削除しています
+                if (collision.gameObject.TryGetComponent(out enemy))
+                {
 
-            // TODO 情報を取得できたら、攻撃状態にする
+                    // 情報を取得できたら、攻撃状態にする
+                    isAttack = true;
 
-            // TODO 攻撃の準備に入る
+                    // 攻撃の準備に入る
+                    StartCoroutine(PrepareteAttack());
+                }
+            }
+        }
+    }
 
+    /// <summary>
+    /// 攻撃準備
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator PrepareteAttack()
+    {
+        Debug.Log("攻撃準備開始");
+
+        int timer = 0;
+
+        // 攻撃中の間だけループ処理を繰り返す
+        while (isAttack)
+        {
+            // TODO ゲームプレイ中のみ攻撃する
+
+            timer++;
+
+            // 攻撃のための待機時間が経過したら    
+            if (timer > intervalAttackTime)
+            {
+
+                // 次の攻撃に備えて、待機時間のタイマーをリセット
+                timer = 0;
+
+                // 攻撃
+                Attack();
+
+                // TODO 攻撃回数関連の処理をここに記述する
+            }
+
+            // １フレーム処理を中断する(この処理を書き忘れると無限ループになり、Unity エディターが動かなくなって再起動することになります。注意！)
+            yield return null;
+        }
+    }
+
+    /// <summary>
+    /// 攻撃
+    /// </summary>
+    private void Attack()
+    {
+        Debug.Log("攻撃");
+        // TODO キャラの上に攻撃エフェクトを生成
+        // 敵キャラ側に用意したダメージ計算用のメソッドを呼び出して、敵にダメージを与える
+        enemy.CulcDamage(attackPower);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Enemy")
+        {
+            Debug.Log("敵なし");
+
+            isAttack = false;
+            enemy = null;
         }
     }
 }
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -13,6 +13,14 @@ public class EnemyController : MonoBehaviour
     [SerializeField, Header("移動速度")]
     private float moveSpeed;
 
+    [SerializeField, Header("最大HP")]
+    private int maxHp;
+
+    [SerializeField]
+    private int hp;
+
+    private Tween tween;
+
     private Vector3[] paths;
 
     private Animator anim;　　　　　　 // Animator コンポーネントの取得用
@@ -20,14 +28,17 @@ public class EnemyController : MonoBehaviour
 
     void Start()
     {
+        hp = maxHp;
+
         // Animator コンポーネントを取得して anim 変数に代入
         TryGetComponent(out anim);
 
         // 移動する地点を取得
         paths = pathData.pathTranArray.Select(x => x.position).ToArray();
 
-        // 各地点に向けて移動
-        transform.DOPath(paths, 1000 / moveSpeed).SetEase(Ease.Linear).OnWaypointChange(ChangeAnimeDirection);
+        // 各地点に向けて移動。今後この処理を制御するため、Tween 型の変数に DOPath メソッドの処理を代入しておく
+        tween = transform.DOPath(paths, 1000 / moveSpeed).SetEase(Ease.Linear).OnWaypointChange(ChangeAnimeDirection);    //  <=  DOPath の処理を tween 変数に代入します
+
     }
 
     /// <summary>
@@ -49,6 +60,48 @@ public class EnemyController : MonoBehaviour
         // アニメーションの Palameter の値を更新し、移動アニメの BlendTree を制御して移動の方向と移動アニメを同期
         anim.SetFloat("X", direction.x);
         anim.SetFloat("Y", direction.y);
+    }
 
+    /// <summary>
+    /// ダメージ計算
+    /// </summary>
+    /// <param name="amount"></param>
+    public void CulcDamage(int amount)
+    {
+
+        // Hp の値を減算した結果値を、最低値と最大値の範囲内に収まるようにして更新
+        hp = Mathf.Clamp(hp -= amount, 0, maxHp);
+
+        Debug.Log("残りHP : " + hp);
+
+        // Hp が 0 以下になった場合
+        if (hp <= 0)
+        {
+
+            // 破壊処理を実行するメソッドを呼び出す
+            DestroyEnemy();
+        }
+
+        // TODO 演出用のエフェクト生成
+
+
+        // TODO ヒットストップ演出
+
+    }
+
+    /// <summary>
+    /// 敵破壊処理
+    /// </summary>
+    public void DestroyEnemy()
+    {
+        // Kill メソッドを実行し、tween 変数に代入されている処理(DOPath の処理)を終了する
+        tween.Kill();
+
+        // TODO SEの処理
+
+        // TODO 破壊時のエフェクトの生成や関連する処理
+
+        // 敵キャラの破壊
+        Destroy(gameObject);
     }
 }
