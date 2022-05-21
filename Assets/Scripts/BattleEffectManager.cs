96c05a5ea8a89f6099e1d630d92b6fed490a5d33
八木暁史 <>
2022-05-21 19:56:30 +0900

    発展１０　ーエフェクト機能の準備ー

---
+++ Assets/Scripts/BattleEffectManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class BattleEffectManager : MonoBehaviour
+{
+    public static BattleEffectManager instance;
+
+    // キャラ 用
+    public GameObject destroyCharaEffectPrefab;
+
+    // エネミー用
+    public GameObject destroyEnemyEffectPrefab;
+    public GameObject hitEnemyEffectPrefab;
+
+    // 拠点用
+    public GameObject hitDefenseBaseEffectPrefab;
+
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    /// <summary>
+    /// EffectType で指定したエフェクトのプレファブを取得
+    /// </summary>
+    /// <param name="effectType"></param>
+    /// <returns></returns>
+    public GameObject GetEffect(EffectType effectType)
+    {
+
+        // 引数の値で分岐し、該当した EffectType のエフェクトの情報を戻す
+        return effectType switch
+        {
+
+            // 【チェックする effectType 変数の値】 => 【該当するエフェクトの情報】という書式
+            EffectType.Destroy_Chara => destroyCharaEffectPrefab,
+            EffectType.Destroy_Enemy => destroyEnemyEffectPrefab,
+            EffectType.Hit_Enemy => hitEnemyEffectPrefab,
+            EffectType.Hit_DefenseBase => hitDefenseBaseEffectPrefab,
+            _ => destroyCharaEffectPrefab,
+        };
+    }
+}
---
+++ Assets/Scripts/EffectType.cs
@@ -0,0 +1,13 @@
+/// <summary>
+/// エフェクトの種類
+/// </summary>
+public enum EffectType
+{
+    Destroy_Chara,　　　　　// 配置した味方キャラが破壊される際のエフェクト
+    Destroy_Enemy,          // エネミーを破壊した際のエフェクト
+    Hit_Enemy,              // 味方キャラの攻撃がエネミーにヒットした際の、エネミー側の被ダメージのエフェクト
+    Hit_DefenseBase,        // 防衛拠点がエネミーの攻撃を受けた際のエフェクト
+
+    // TODO 他にもあれば追加
+
+}
