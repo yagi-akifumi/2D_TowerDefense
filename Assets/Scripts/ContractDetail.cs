08b0374b62ac3ce2815d58d7faa44fac513d80ce
八木暁史 <>
2022-05-26 00:19:34 +0900

    発展１６　ー契約時のエフェクト機能の実装ー

---
+++ Assets/Scripts/ContractDetail.cs
@@ -0,0 +1,100 @@
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+/// <summary>
+/// 契約演出用のクラス
+/// </summary>
+public class ContractDetail : MonoBehaviour
+{
+    [SerializeField]
+    private Image imgChara;
+
+    [SerializeField]
+    private Text txtCharaName;
+
+    [SerializeField]
+    private Button btnSubmitContractStamp;
+
+    [SerializeField]
+    private Button btnFilter;
+
+    [SerializeField]
+    private CanvasGroup canvasGrouContractSet;
+
+    [SerializeField]
+    private CanvasGroup canvasGroupSubmitContractStamp;
+
+    [SerializeField]
+    private Image imgContractStamp;
+
+
+    /// <summary>
+    /// 契約演出の設定
+    /// </summary>
+    /// <param name="charaData"></param>
+    public void SetUpContractDetail(CharaData charaData)
+    {
+
+        // 契約したキャラの画像を設定
+        imgChara.sprite = charaData.charaSprite;
+
+        // 契約したキャラの名前を決定
+        txtCharaName.text = charaData.charaName;
+
+        // ContractSet にアタッチされている CanvasGroup のアルファを 0 にする
+        canvasGrouContractSet.alpha = 0;
+
+        // 順番にボタンを押せるように、あとから表示するスタンプの画像を見えないように設定し、タップ感知しないようにしておく
+        canvasGroupSubmitContractStamp.alpha = 0;
+        canvasGroupSubmitContractStamp.blocksRaycasts = false;
+        imgContractStamp.enabled = false;
+
+        // 各ボタンにメソッドを登録
+        btnFilter.onClick.AddListener(OnClickFilter);
+        btnSubmitContractStamp.onClick.AddListener(OnClickSubmitContract);
+
+        // 0 にしておいた、最初のボタン用に CanvasGruop を表示
+        canvasGrouContractSet.DOFade(1.0f, 0.5f).SetEase(Ease.Linear);
+    }
+
+    /// <summary>
+    /// スタンプ前にタップした際の処理
+    /// </summary>
+    private void OnClickFilter()
+    {
+
+        // スタンプを動かす
+        // スタンプのサイズを３倍にする(サイズの大きくなっているもの小さくする演出につかうため)
+        imgContractStamp.transform.localScale = Vector3.one * 3;
+
+        // スタンプの角度をランダムに設定する(捺印の位置が毎回変わるようにする演出)
+        imgContractStamp.transform.eulerAngles = new Vector3(0, 0, Random.Range(-30.0f, 30.0f));
+
+        // スタンプの画像を表示
+        imgContractStamp.enabled = true;
+
+        // CanvasGroup の透明度を制御
+        canvasGroupSubmitContractStamp.alpha = 1.0f;
+
+        // スタンプを元の大きさに戻す。
+        // Easeの設定により、元のサイズに戻ってから少しだけ大きくして戻すことで、スタンプを捺しているように見せる
+        imgContractStamp.transform.DOScale(Vector3.one, 0.75f)
+            .SetEase(Ease.OutBack, 1.0f)
+            .OnComplete(() =>
+            {
+                // ボタン(画面)を押せるようにする
+                canvasGroupSubmitContractStamp.blocksRaycasts = true;
+            }
+        );
+    }
+
+    /// <summary>
+    /// スタンプ後にタップした際の処理
+    /// </summary>
+    private void OnClickSubmitContract()
+    {
+        // 契約演出を終了して、ポップアップも閉じる
+        canvasGrouContractSet.DOFade(0.0f, 0.5f).SetEase(Ease.Linear).OnComplete(() => { Destroy(gameObject); });
+    }
+}
