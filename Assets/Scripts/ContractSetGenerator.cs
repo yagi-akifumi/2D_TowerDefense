b0aee783c005c4bddf485c63d2af84abe70b6a97
八木暁史 <>
2022-05-29 16:49:34 +0900

    画面表示の実装チャレンジ

---
+++ Assets/Scripts/ContractSetGenerator.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContractSetGenerator : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject ContractSet;
+
+    [SerializeField]
+    private Transform canvasTran;
+
+    [SerializeField,Header("管理画面の表示")]
+    private bool ContractSetPreview;
+
+    [SerializeField,Header("スタンプの表示")]
+    private bool StampPreview;
+
+    void start()
+    {
+        ContractSetPreview = false;
+        StampPreview = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (ContractSetPreview == false)
+            {
+                ActivateContractSet();
+            }
+
+            else
+            if(ContractSetPreview == true)
+            {
+                InactivateContractSet();
+            }
+        }
+    }
+
+    /// <summary>
+    /// 表示
+    /// </summary>
+    public void ActivateContractSet()
+    {
+        // ポップアップを生成
+        Debug.Log("ゲーム表示");
+        // コントラクトセットをキャンバスにインスタンス化する
+        ContractSet = Instantiate(ContractSet, canvasTran, false);
+        // コントラクトセットを表示する
+        ContractSet.gameObject.SetActive(true);
+        // ContractSetPreviewをtrueにする
+        ContractSetPreview = true;
+    }
+
+    /// <summary>
+    /// 非表示
+    /// </summary>
+    public void InactivateContractSet()
+    {
+        // ポップアップを生成
+        Debug.Log("ゲーム非表示");
+        // コントラクトセットを非表示にする
+        ContractSet.gameObject.SetActive(false);
+        // ContractSetPreviewをfalseにする
+        ContractSetPreview = false;
+    }
+}
