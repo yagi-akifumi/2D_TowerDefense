89cf033f2e4ac7b56566044fac8927b93692f70f
八木暁史 <>
2022-06-02 01:05:15 +0900

    btnChooseChara.onClick.AddListener(OnClickSubmitChooseChara2);→ボタン実装もうまくいっていない。（デバッグログも出ない）。明日要確認。

---
+++ Assets/Scripts/world_PlacementCharaSelectPopUp.cs
@@ -0,0 +1,120 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class world_PlacementCharaSelectPopUp : MonoBehaviour
+{
+    [SerializeField]
+    private Button btnClosePopUp;
+
+    [SerializeField]
+    private Button btnChooseChara;
+
+    [SerializeField]
+    private CanvasGroup canvasGroup;
+
+    private CharaGenerator charaGenerator;
+
+
+    // TODO 制御を行いたい各コンポーネントの情報をアサインするための変数群を追加する
+
+
+    /// <summary>
+    /// ポップアップの設定
+    /// </summary>
+    /// <param name="charaGenerator"></param>
+    public void SetUpPlacementCharaSelectPopUp2(CharaGenerator charaGenerator)
+    {
+
+        this.charaGenerator = charaGenerator;
+
+        // TODO 他に設定項目があったら追加する
+
+
+        // ポップアップを一度見えない状態にする
+        canvasGroup.alpha = 0;
+
+        // 各ボタンの操作を押せない状態にする
+        SwithcActivateButtons(false);
+
+
+        // TODO スクリプタブル・オブジェクトに登録されているキャラ分のボタンのゲームオブジェクトを生成
+
+
+        // TODO 最初に生成したボタンの場合
+
+
+        // TODO 選択しているキャラとして初期値に設定
+
+
+        // 各ボタンにメソッドを登録
+        btnChooseChara.onClick.AddListener(OnClickSubmitChooseChara2);
+
+        btnClosePopUp.onClick.AddListener(OnClickClosePopUp2);
+
+        // 各ボタンを押せる状態にする
+        SwithcActivateButtons(true);
+    }
+
+    /// <summary>
+    /// 各ボタンのアクティブ状態の切り替え
+    /// </summary>
+    /// <param name="isSwitch"></param>
+    public void SwithcActivateButtons(bool isSwitch)
+    {
+        btnChooseChara.interactable = isSwitch;
+        btnClosePopUp.interactable = isSwitch;
+    }
+
+    /// <summary>
+    /// ポップアップの表示
+    /// </summary>
+    public void ShowPopUp()
+    {
+
+        // TODO 各キャラのボタンの制御
+
+
+        // ポップアップの表示
+        canvasGroup.DOFade(1.0f, 0.5f);
+    }
+
+    /// <summary>
+    /// 選択しているキャラを配置するボタンを押した際の処理
+    /// </summary>
+    private void OnClickSubmitChooseChara2()
+    {
+
+        Debug.Log("雇用契約");
+
+        // ポップアップの非表示
+        HidePopUp2();
+    }
+
+    /// <summary>
+    /// 配置を止めるボタンを押した際の処理
+    /// </summary>
+    private void OnClickClosePopUp2()
+    {
+
+        Debug.Log("画面を閉じる1");
+        // ポップアップの非表示
+        HidePopUp2();
+    }
+
+    /// <summary>
+    /// ポップアップの非表示
+    /// </summary>
+    private void HidePopUp2()
+    {
+
+        Debug.Log("画面を閉じる2");
+        // TODO 各キャラのボタンの制御
+
+
+        // ポップアップの非表示
+        //canvasGroup.DOFade(0, 0.5f).OnComplete(() => charaGenerator.InactivatePlacementCharaSelectPopUp());  // 次の手順でメソッドを追加するので、それまでコメントアウトしておいてください。
+    }
+}
