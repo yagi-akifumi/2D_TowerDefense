c2eb45eeec628f038b09ccdff349dba95e9ba266
八木暁史 <>
2022-06-04 07:21:03 +0900

    worldCharaGeneratorをスクリプトの設定処理を実行する

---
+++ Assets/Scripts/WorldGameManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGameManager : MonoBehaviour
+{
+    [SerializeField]
+    private WorldCharaGenerator worldCharaGenerator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //worldCharaGeneratorをスクリプトの設定処理を実行する
+        // キャラ配置用ポップアップの生成と設定
+        
+        //StartCoroutine(worldCharaGenerator.SetUpCharaGenerator(this));
+    }
+}
